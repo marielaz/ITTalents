531d56d64a4bf6d163df68c8242e2f04e9246701
marielaz <m.zviskova11@gmail.com>
2017-03-02 11:56:25 +0200

    Update Drink.java

--- products/Drink.java
+++ products/Drink.java
@@ -2,13 +2,27 @@ package products;
 
 public class Drink extends Product {
 
-	enum DrinkType { COLA, FANTA, SPRITE };
-	
+	enum DrinkType {
+		COLA(2.8), FANTA(2.8), SPRITE(2.8), NESTEA(2);
+
+		private double price;
+
+		private DrinkType(double price) {
+			this.price = price;
+		}
+
+		public double getPrice() {
+			return price;
+		}
+
+	};
+
 	private DrinkType drinkType;
-	
-	public Drink(String name, double price, DrinkType drinkType) {
-		super(name, price);
+
+	public Drink(String name, DrinkType drinkType) {
+		super(name, drinkType.getPrice());
 		this.drinkType = drinkType;
 	}
 
 }
+
