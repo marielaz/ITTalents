3c581b8df56870e655c7f0461182a2b75e0a4f3e
marielaz <m.zviskova11@gmail.com>
2017-03-02 11:57:01 +0200

    Update Dessert.java

--- products/Dessert.java
+++ products/Dessert.java
@@ -2,14 +2,26 @@ package products;
 
 public class Dessert extends Product {
 
-	enum DessertType { ICE_CREAM, OREO_CAKE, CHEESECAKE };
-	
+	enum DessertType {
+		CHOCO_PIE(6.5), SOUFFLE(6.5), NIRVANA(2.9), MINI_PANCAKES(3.5);
+
+		private double price;
+
+		private DessertType(double price) {
+			this.price = price;
+		}
+
+		public double getPrice() {
+			return price;
+		}
+
+	};
+
 	private DessertType dessertType;
-	
-	public Dessert(String name, double price, DessertType dessertType) {
-		super(name, price);
+
+	public Dessert(String name, DessertType dessertType) {
+		super(name, dessertType.getPrice());
 		this.dessertType = dessertType;
 	}
 
-	
 }
